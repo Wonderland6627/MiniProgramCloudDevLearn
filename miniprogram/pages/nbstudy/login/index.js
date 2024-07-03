7ee6a7c74315cf5a4c1865fc5f5bca74f199fcd1
Wonderland6627 <994015020@qq.com>
2024-07-03 18:58:30 +0800

    使用微信云开发SDK 格式化js

--- miniprogram/pages/nbstudy/login/index.js
+++ miniprogram/pages/nbstudy/login/index.js
@@ -16,23 +16,14 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-    getApp().getOpenID()
-    this.test()
-  },
-
-  async test() {
-    const { data } = await getApp().getModels().students.create({
-      data: {
-        studentName: "hihihi"
-      }
-    })
-    console.log(data)
+    
   },
 
   tryWXLogin() {
     wx.showLoading({
       title: '检查登陆状态',
     })
+    const openid = getApp().getOpenID()
     wx.checkSession({
       success: () => {
         wx.showToast({
@@ -40,9 +31,9 @@ Page({
           icon: 'success',
         })
         console.log('微信session有效')
-        this.createStudent()
+        this.checkStudentDataExists(openid)
       },
-      fail: ()=> {
+      fail: () => {
         wx.showLoading({
           title: '正在登陆',
         })
@@ -55,14 +46,25 @@ Page({
             })
             if (res.code) { //到这一步获取到code代表登陆成功 后面json2Session主要是为了本地存openid
               console.log('微信登陆凭证 code: ' + res.code)
-              this.createStudent()
-              this.setData({ js_code: res.code })
-              const { js_code } = this.data
+              this.setData({
+                js_code: res.code
+              })
+              if (openid !== '') { //如果有openid 直接拉数据进主页
+                console.log('微信openid存在')
+                this.checkStudentDataExists(openid)
+                return
+              }
+              console.log('微信openid不存在，尝试获取')
+              const {
+                js_code
+              } = this.data
               wx.cloud.callFunction({
                 name: 'quickstartFunctions',
                 data: {
                   type: 'getJSCode2Session',
-                  data: { js_code }
+                  data: {
+                    js_code
+                  }
                 },
                 success: (res) => {
                   console.log('微信登陆凭证校验成功: ' + JSON.stringify(res))
@@ -71,7 +73,10 @@ Page({
                     return
                   }
                   console.log('微信登陆凭证校验成功回应: ' + JSON.stringify(res.result.data))
-                  getApp().setOpenID(res.result.data.openid)
+                  const oid = res.result.data.openid
+                  console.log('通过JSCode2Session获取微信openid成功: ' + oid)
+                  getApp().setOpenID(oid) //通过JSCode2Session获取openid
+                  this.checkStudentDataExists(oid)
                 },
                 fail: (err) => {
                   console.error('微信登陆凭证校验失败: ' + err)
@@ -87,6 +92,33 @@ Page({
         })
       }
     })
+  },
+
+  async checkStudentDataExists(openid) {
+    wx.showLoading({
+      title: '检查用户数据',
+    })
+    const result = await getApp().getModels().students.get({
+      filter: {
+        where: {
+          OPENID: {
+            $eq: openid,
+          }
+        }
+      }
+    })
+    const data = result?.data
+    console.log(data)
+    if (data == null) {
+      console.log('openid为：' + openid + '的学生信息不存在，准备创建')
+      //todo create data
+      return
+    }
+    if (data.studentName == null) {
+      console.log('openid为：' + openid + '的学生信息不全，准备补充')
+      //todo jump to onboarding
+      return
+    }
   },
 
   async createStudent() {
@@ -119,14 +151,18 @@ Page({
   },
 
   gotoNext(isNewUser) {
-    
+
   },
 
   async fetchStoresList() {
-    this.setData({ isLoading: true });
+    this.setData({
+      isLoading: true
+    });
     const res = await wx.cloud.callFunction({
       name: 'quickstartFunctions',
-      data: { type: 'fetchStoresList' },
+      data: {
+        type: 'fetchStoresList'
+      },
     });
     const storesList = res?.result?.dataList || [];
     this.setData({
@@ -135,7 +171,7 @@ Page({
     });
   },
 
-  tryCustomLogin: function() {
+  tryCustomLogin: function () {
     console.log("try custom login")
     //this.fetchStoresList();
     this.customLogin();
@@ -146,17 +182,21 @@ Page({
 
   async customLogin() {
     // this.setData({ isLoading: true });
-    const { phoneNumber, password } = this.data;
+    const {
+      phoneNumber,
+      password
+    } = this.data;
     console.log(phoneNumber)
     console.log(password)
     const response = await wx.cloud.callFunction({
-        name: 'quickstartFunctions',
+      name: 'quickstartFunctions',
+      data: {
+        type: 'checkLogin',
         data: {
-          type: 'checkLogin',
-          data: {
-            phoneNumber, password
-          }
+          phoneNumber,
+          password
         }
+      }
     });
     const result = response?.result;
     console.log("登陆回应：" + JSON.stringify(result))
@@ -181,11 +221,11 @@ Page({
     });
   },
 
-  contactUs: function() {
+  contactUs: function () {
     console.log("contact us")
   },
 
-  checkPhoneNumber: function(e) {
+  checkPhoneNumber: function (e) {
     const phone = e.detail.value
     console.log(phone)
     this.setData({
