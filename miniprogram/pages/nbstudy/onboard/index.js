0901c05205d5bdb04be1e7fed5b1b0d12e64f17a
Wonderland6627 <994015020@qq.com>
2024-07-04 18:30:50 +0800

    上传头像并保存在云资源中 获取下来cdn链接

--- miniprogram/pages/nbstudy/onboard/index.js
+++ miniprogram/pages/nbstudy/onboard/index.js
@@ -8,8 +8,9 @@ Page({
    * 页面的初始数据
    */
   data: {
-    avatarUrl: defaultAvatarUrl,
-    studentData: {},
+    studentData: {
+      avatarUrl: defaultAvatarUrl,
+    },
   },
 
   /**
@@ -20,8 +21,32 @@ Page({
     this.setData({
       studentData: data,
     })
-    console.log(data)
+    console.log('获取studentAccountData: ' + data)
     wx.removeStorageSync('studentAccountData')
+  },
+
+  onChooseAvatar(e) {
+    var filePath = e.detail.avatarUrl
+    console.log('获取头像变化: ' + filePath)
+    var clouthPath = 'studentAvatars/avatar_' + getApp().getOpenID() + '.png'
+    console.log(clouthPath)
+    wx.cloud.uploadFile({
+      cloudPath: clouthPath,
+      filePath: filePath
+    }).then(res => {
+      console.log('头像上传成功: ' + filePath)
+      wx.cloud.getTempFileURL({
+        fileList: [res.fileID]
+      }).then(res => {
+        var avatarUrl = res.fileList[0].tempFileURL + '?t=' + new Date().getTime()
+        console.log('头像CloudURL: ' + avatarUrl)
+        this.setData({
+          'studentData.avatarUrl': avatarUrl
+        })
+      })
+    }).catch(e => {
+      console.log('头像上传失败: ' + e)
+    })
   },
 
   /**
