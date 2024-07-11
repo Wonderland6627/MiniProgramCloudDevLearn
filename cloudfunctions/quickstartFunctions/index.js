9f882b92f25aef50321db94c50b92048c1f184d4
Wonderland6627 <994015020@qq.com>
2024-07-11 23:30:44 +0800

    删除模板自带资源 压缩包体 第一次上传 v0.0.1

--- cloudfunctions/quickstartFunctions/index.js
+++ cloudfunctions/quickstartFunctions/index.js
@@ -1,11 +1,4 @@
 const getOpenId = require('./getOpenId/index');
-const getMiniProgramCode = require('./getMiniProgramCode/index');
-const createCollection = require('./createCollection/index');
-const selectRecord = require('./selectRecord/index');
-const updateRecord = require('./updateRecord/index');
-const sumRecord = require('./sumRecord/index');
-const fetchGoodsList = require('./fetchGoodsList/index');
-const genMpQrcode = require('./genMpQrcode/index');
 
 const fetchStoresList = require('./fetchStoresList/index')
 const checkLogin = require('./checkLogin/index');
@@ -18,20 +11,6 @@ exports.main = async (event, context) => {
   switch (event.type) {
     case 'getOpenId':
       return await getOpenId.main(event, context);
-    case 'getMiniProgramCode':
-      return await getMiniProgramCode.main(event, context);
-    case 'createCollection':
-      return await createCollection.main(event, context);
-    case 'selectRecord':
-      return await selectRecord.main(event, context);
-    case 'updateRecord':
-      return await updateRecord.main(event, context);
-    case 'sumRecord':
-      return await sumRecord.main(event, context);
-    case 'fetchGoodsList':
-      return await fetchGoodsList.main(event, context);
-    case 'genMpQrcode':
-      return await genMpQrcode.main(event, context);
       
     case 'fetchStoresList':
       return await fetchStoresList.main(event, context);
