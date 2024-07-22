879091e1db2520f28c807e9af4e232709d7e3779
Wonderland6627 <994015020@qq.com>
2024-07-22 21:40:00 +0800

    更新整理云函数列表

--- cloudfunctions/quickstartFunctions/index.js
+++ cloudfunctions/quickstartFunctions/index.js
@@ -1,11 +1,14 @@
 const getOpenId = require('./getOpenId/index');
 
-const fetchStoresList = require('./fetchStoresList/index')
 const checkLogin = require('./checkLogin/index');
 const getJSCode2Session = require('./getJScode2Session/index')
 
 const createStudent = require('./students/createStudent/index')
-const updatePwd = require('./updatePwd/index')
+
+const fetchStoresList = require('./stores/fetchStoresList/index')
+const updatePwd = require('./stores/updatePwd/index')
+
+const updatePackages = require('./packages_collection/updatePackages/index');
 
 // 云函数入口函数
 exports.main = async (event, context) => {
@@ -13,8 +16,6 @@ exports.main = async (event, context) => {
     case 'getOpenId':
       return await getOpenId.main(event, context);
       
-    case 'fetchStoresList':
-      return await fetchStoresList.main(event, context);
     case 'checkLogin':
       return await checkLogin.main(event, context);
     case 'getJSCode2Session':
@@ -25,6 +26,11 @@ exports.main = async (event, context) => {
 
     case 'updatePwd': 
       return await updatePwd.main(event, context);
+    case 'fetchStoresList':
+      return await fetchStoresList.main(event, context);
+
+    case 'updatePackages':
+      return await updatePackages.main(event, context);
   }
 };
         
