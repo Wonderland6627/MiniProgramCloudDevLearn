deedf926043f0b67e387f24f180102ea42d55959
Wonderland6627 <994015020@qq.com>
2024-08-03 23:37:32 +0800

    编辑器log输出

--- miniprogram/logger.js
+++ miniprogram/logger.js
@@ -1,19 +1,24 @@
 var logManager = wx.getRealtimeLogManager ? wx.getRealtimeLogManager() : null
+var enableEditorLog = true
 
 module.exports = {
   debug() {
+    if (enableEditorLog) console.debug(arguments[0])
     if (!logManager) return
     logManager.debug.apply(logManager, arguments)
   },
   info() {
+    if (enableEditorLog) console.info(arguments[0])
     if (!logManager) return
     logManager.info.apply(logManager, arguments)
   },
   warn() {
+    if (enableEditorLog) console.warn(arguments[0])
     if (!logManager) return
     logManager.warn.apply(logManager, arguments)
   },
   error() {
+    if (enableEditorLog) console.error(arguments[0])
     if (!logManager) return
     logManager.error.apply(logManager, arguments)
   },
