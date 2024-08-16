6e825c5981c4eb1b8e25bf4fa45183bd3414ab18
Wonderland6627 <994015020@qq.com>
2024-08-16 17:04:13 +0800

    同步修改数据库枚举类型

--- miniprogram/consts.js
+++ miniprogram/consts.js
@@ -1,13 +1,38 @@
 // consts.js
 
 //与后台枚举选项内容要保持一致
+
+//座位相关
 const SeatType = {
+	Empty: 'Empty',
+  VIP: 'VIP',
   A: 'A',
   B: 'B',
   C: 'C',
-  VIP: 'VIP',
+}
+const SeatArray = [
+	SeatType.Empty,
+	SeatType.VIP,
+	SeatType.A,
+	SeatType.B,
+	SeatType.C,
+]
+const SeatTypeRemote2Local = {
+	0: 'Empty',
+  1: 'VIP',
+  2: 'A',
+  3: 'B',
+  4: 'C',
+}
+const SeatTypeLocal2Remote = {
+	'Empty': '0',
+  'VIP': '1',
+  'A': '2',
+  'B': '3',
+  'C': '4',
 }
 
+//套餐时长相关
 const DurationType = {
   Temp: 'Temp',
   Week: 'Week',
@@ -15,40 +40,25 @@ const DurationType = {
   Season: 'Season',
   Year: 'Year',
 }
-
-const SeatTypeRemote2Local = {
-  1: 'A',
-  2: 'B',
-  3: 'C',
-  4: 'VIP',
-}
-
 const DurationTypeRemote2Local = {
-  1: 'Temp',
-  2: 'Week',
-  3: 'Month',
-  4: 'Season',
-  5: 'Year',
+  0: 'Temp',
+  1: 'Week',
+  2: 'Month',
+  3: 'Season',
+  4: 'Year',
 }
-
-const SeatTypeLocal2Remote = {
-  'A': '1',
-  'B': '2',
-  'C': '3',
-  'VIP': '4',
-}
-
 const DurationTypeLocal2Remote = {
-  'Temp': '1',
-  'Week': '2',
-  'Month': '3',
-  'Season': '4',
-  'Year': '5',
+  'Temp': '0',
+  'Week': '1',
+  'Month': '2',
+  'Season': '3',
+  'Year': '4',
 }
 
 module.exports = {
   SeatType,
-  DurationType,
+	DurationType,
+	SeatArray,
   SeatTypeRemote2Local,
   DurationTypeRemote2Local,
   SeatTypeLocal2Remote,
